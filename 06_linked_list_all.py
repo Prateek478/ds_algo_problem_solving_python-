fbe4a3db1c0e4bff5b574bf9264ba58de0db7ec0
Prateek478 <prateek.chandil@gmail.com>
2019-12-10 16:50:54 +0530

    Create 06_linked_list_all.py

---
+++ 06_linked_list_all.py
@@ -0,0 +1,12 @@
+'''
+Linked List construction, lets do all these as part of this exercise 
+  1. InsertBeggining
+  2. InsertEnd
+  3. InsertAtK
+  4. DeleteBeginning
+  5. DeleteEnd
+  6. DeleteAtK
+  7. PrintFromBeggining
+  8. PrintFromEnd
+  9. Reverse linked list
+'''
