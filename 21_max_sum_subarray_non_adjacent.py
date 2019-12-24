4dbf8f246637079d738a304fba474d0aa36380d1
Prateek478 <prateek.chandil@gmail.com>
2019-12-24 14:44:38 +0530

    Create 21_max_sum_subarray_non_adjacent.py

---
+++ 21_max_sum_subarray_non_adjacent.py
@@ -0,0 +1,7 @@
+'''
+Given an array of integers, find the maximum sum of subsequence of given array where subsequence contains
+no adjacent elements.
+Example:
+Input: {1,2,9,4,5,0,4,11,6}
+output: Maximum sum is 26 formed by the subsequence {1,9,5,11}
+'''
