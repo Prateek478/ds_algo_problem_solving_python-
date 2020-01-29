fbc4ec0bb240cd944cbcc300ca5206580968ef38
Prateek478 <prateek.chandil@gmail.com>
2020-01-29 11:25:36 +0530

    Create 28_lowest_common_ancestor.py

---
+++ 28_lowest_common_ancestor.py
@@ -0,0 +1,9 @@
+'''
+Today’s Problem: Lowest Common Ancestor in a Binary Tree
+
+Given a binary tree (not a binary search tree) and two values say n1 and n2, write a program to find the least common ancestor.
+
+Following is definition of LCA from Wikipedia:
+Let T be a rooted tree. The lowest common ancestor between two nodes n1 and n2 is defined as the lowest node in T that has both 
+n1 and n2 as descendants (where we allow a node to be a descendant of itself).
+'''
