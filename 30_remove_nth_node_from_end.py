46cc88341ce41ca4bfcb61ace339803549d7f42d
Prateek478 <prateek.chandil@gmail.com>
2020-02-10 10:37:31 +0530

    Create 30_remove_nth_node_from_end.py

---
+++ 30_remove_nth_node_from_end.py
@@ -0,0 +1,10 @@
+'''
+Delete Nth node from the end of the given linked list
+Input: 2 -> 3 -> 1 -> 7 -> NULL, N = 1  
+Output:
+Created Linked list is:
+2 3 1 7
+Linked List after Deletion is:
+2 3 1
+'''
+
